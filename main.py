e722a105a2dbfc8a2c6127c5228ba443aee89631
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-03-07 15:02:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,45 @@ def subp(cmd):
   f.close()
   return result
 
+
+def convert_video(chat_id, input_type, output_type):
+
+	"""
+
+	The function converts video of one type to another.
+
+	Args:
+
+		chat_id: unique identification for video
+
+		input_type: video input type
+
+		output_type: video output type
+
+	"""
+
+	inputs = {'./input_media/{}.{}'.format(chat_id, input_type): None}
+
+	if output_type == "gif":
+
+		outputs = {'./output_media/{}.{}'.format(chat_id, output_type): '-t 3 -vf "fps=30,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0'}
+
+	else:
+
+		outputs = {'./output_media/{}.{}'.format(chat_id, output_type): None}
+
+	ff = ffmpy.FFmpeg(
+
+	    inputs=inputs,
+
+	    outputs=outputs
+
+	)
+
+	ff.run()
+
+	return None 
+
 def generate_thumbnail(in_filename):
     probe = ffmpeg.probe(in_filename)
     out_filename = f'{in_filename[:-3]}.jpg'
